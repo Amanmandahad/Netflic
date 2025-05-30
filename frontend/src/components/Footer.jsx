b434c810a8f8927e798df147b0fc7db67f5e8273
Amanmandahad <mandahadaman>
2025-05-30 14:26:40 +0530

    Updated fontsize

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,23 +1,26 @@
 const Footer = () => {
-	return (
-		<footer className='py-6 px-4 bg-black text-white border-t border-gray-800'>
-			<div className='flex flex-col items-center justify-center text-center'>
-				<p className='text-sm leading-loose text-muted-foreground'>
-					Built by{" "}
-					<span className='font-semibold text-white'>Aman Mandahad</span>. The source code is available on{" "}
-					<a
-						href='https://github.com/Amanmandahad'
-						target='_blank'
-						rel='noreferrer'
-						className='font-medium underline underline-offset-4 text-white'
-					>
-						GitHub
-					</a>
-					.
-				</p>
-			</div>
-		</footer>
-	);
+  return (
+    <footer className="py-6 px-4 bg-black text-white border-t border-gray-800">
+      <div className="flex flex-col items-center justify-center text-center">
+        <p className="text-sm leading-loose text-muted-foreground">
+          Built by{" "}
+          <span className="font-semibold text-white">Aman Mandahad</span>.
+          <br className="block md:hidden" />{" "}
+          {/* Only show line break on small screens */}
+          The source code is available on{" "}
+          <a
+            href="https://github.com/Amanmandahad"
+            target="_blank"
+            rel="noreferrer"
+            className="font-medium underline underline-offset-4 text-white"
+          >
+            GitHub
+          </a>
+          .
+        </p>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
